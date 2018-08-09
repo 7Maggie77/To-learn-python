c28af9871ab21966b94678c00f43bf694ce83aaf
Maggie <767983649@qq.com>
2018-08-09 18:07:57 +0800

    8.9 learing

---
+++ 8.9.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+'''if 语句
+height=input('请输入身高：')
+weight=input('请输入体重：')
+bmi=float(weight)/(float(height)**2)
+if(bmi<18.5):
+    print("过轻")
+elif(bmi<25):
+    print("正常")
+elif(bmi<28):
+    print("过重")
+elif(bmi<32):
+    print("肥胖")
+else:
+    print("严重肥胖")
+'''
+
+L=['ABBEY','DORA','MAGGIE']
+for name in L:
+   print('Hello, %s!' %name)
